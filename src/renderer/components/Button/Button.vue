491980a49e9736ba36861117e83a97c024bfd1db
Fennie <beduo@qq.com>
2019-01-11 15:52:29 +0800

    components: button

---
+++ src/renderer/components/Button/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <a
+    class="button"
+    :href="href"
+    :disabled="disabled"
+    :target="target"
+    :rel="newRel"
+    @click="$emit('click', $event)"
+    v-if="href"
+  >
+    <slot></slot>
+  </a>
+
+  <button
+    class="button"
+    :type="type"
+    :disabled="disabled"
+    @click="$emit('click', $event)"
+    v-else
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "my-button",
+  props: {
+    href: String,
+    target: String,
+    rel: String,
+    type: {
+      type: String,
+      default: "button"
+    },
+    disabled: Boolean
+  },
+  computed: {
+    newRel() {
+      if (this.target === "_blank") {
+        return this.rel || "noopener"
+      }
+
+      return this.rel
+    }
+  }
+};
+</script>
+<style lang="less" src="./button.less"></style>
