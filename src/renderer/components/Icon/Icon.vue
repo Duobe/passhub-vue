d6073c8826bb36f40a05f84212fdc39db09c2c01
Fennie <beduo@qq.com>
2019-01-12 21:25:40 +0800

    components: icon, use vue-feather-icons-ssr

---
+++ src/renderer/components/Icon/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="icon">
+    <feather-icon :name="name" :size="size" :strokeWidth="strokeWidth"></feather-icon>
+  </span>
+</template>
+<script>
+export default {
+  name: 'ph-icon',
+  props: {
+    name: {
+      type: String
+    },
+    size: {
+      type: String,
+      default: '18'
+    },
+    strokeWidth: {
+      type: String,
+      default: '2'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import url("../../styles/theme.less");
+
+svg {
+  stroke: @grey1;
+}
+</style>
---
+++ src/renderer/components/Icon/index.js
@@ -0,0 +1,5 @@
+import Icon from './Icon.vue'
+
+export default function install (Vue) {
+  Vue.component('ph-icon', Icon)
+}
