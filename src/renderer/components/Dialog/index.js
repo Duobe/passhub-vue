b4a3757b6bbc5da8391402f6f989f8beb3374ecd
Fennie <beduo@qq.com>
2019-02-05 21:58:39 +0800

    removes

--- src/renderer/components/Dialog/index.js
+++
@@ -1,5 +0,0 @@
-import Dialog from './Dialog.vue'
-
-export default function install (Vue) {
-  Vue.component('ph-dialog', Dialog)
-}
--- src/renderer/components/Form/index.js
+++
@@ -1,7 +0,0 @@
-import Form from './Form.vue'
-import FormItem from './FormItem.vue'
-
-export default function install (Vue) {
-  Vue.component('ph-form', Form)
-  Vue.component('ph-form-item', FormItem)
-}
